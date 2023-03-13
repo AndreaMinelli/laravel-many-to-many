ea23b27ada85995f42ade9c3733bc1786a74b53c
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-13 17:26:36 +0100

    Created technologies.edit

---
+++ resources/views/admin/technologies/edit.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Modify tecnology')
+
+@section('content')
+    <h1 class="text-center my-4">Modifica tecnologia</h1>
+    @include('includes.technologies.form')
+@endsection
