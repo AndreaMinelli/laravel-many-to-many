d88886c9f45a5374be8a6689972383d1f115e165
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-13 17:26:25 +0100

    Created technologies.create

---
+++ resources/views/admin/technologies/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Add technology')
+
+@section('content')
+    <h1 class="text-center my-4">Aggiungi tecnologia</h1>
+    @include('includes.technologies.form')
+@endsection
