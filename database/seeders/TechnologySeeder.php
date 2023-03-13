34af3cfa306c5f0b489f458fc4330726e43314a9
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-13 15:11:51 +0100

    Created TechnologySeeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['HTML', 'CSS', 'Javascript', 'Vue', 'PHP', 'Laravel', 'Bootstrap'];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->save();
+        }
     }
 }
