737420b240ec2df7e192b361d993c26a23db356e
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-13 17:04:10 +0100

    Modified technologies.index

--- resources/views/admin/technologies/index.blade.php
+++ resources/views/admin/technologies/index.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
     <h1 class="text-center my-4">Tecnlogie utilizzate</h1>
-    <div class="d-flex justify-content-between align-items-center my-4">
+    <div class="d-flex justify-content-end align-items-center my-3">
         <a class="btn btn-success" href="{{ route('admin.technologies.create') }}">Aggiungi tecnologia</a>
     </div>
     <table class="table table-striped align-middle">
@@ -29,9 +29,6 @@
                     <td>{{ $technology->updated_at }}</td>
                     <td>
                         <div class="d-flex justify-content-end">
-                            <a class="btn btn-primary btn-sm"
-                                href="{{ route('admin.technologies.show', $technology->id) }}"><i
-                                    class="fa-solid fa-eye"></i></a>
                             <a class="btn btn-warning btn-sm text-white mx-3"
                                 href="{{ route('admin.technologies.edit', $technology->id) }}"><i
                                     class="fa-solid fa-pencil"></i></a>
